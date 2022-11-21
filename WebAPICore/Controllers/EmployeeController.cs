5a131b54cf46c5c9534fd066cf9d090be71ab592
Kp1423 <114837729+Kp1423@users.noreply.github.com>
2022-11-20 18:39:42 -0600

    Added Folders

---
+++ WebAPICore/Controllers/EmployeeController.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+
+        public EmployeeController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                    select EmployeeId, EmployeeName, Department,
+                    convert(varchar(10),DateOfJoining,120) as DateOfJoining
+                    ,PhotoFileName
+                    from dbo.Employee
+                    ";
+            DataTable dt = SelectUpdateDelete(query);
+            return new JsonResult(dt);
+        }
+
+
+        [HttpPost]
+        public JsonResult Post(Employee emp)
+        {
+            string query = @"
+                    insert into dbo.Employee 
+                    (EmployeeName,Department,DateOfJoining,PhotoFileName)
+                    values 
+                    (
+                    '" + emp.EmployeeName + @"'
+                    ,'" + emp.Department + @"'
+                    ,'" + emp.DateOfJoining + @"'
+                    ,'" + emp.PhotoFileName + @"'
+                    )
+                    ";
+            DataTable dt = SelectUpdateDelete(query);
+            return new JsonResult("Added Successfully");
+        }
+
+
+        [HttpPut]
+        public JsonResult Put(Employee emp)
+        {
+            string query = @"
+                    update dbo.Employee set 
+                    EmployeeName = '" + emp.EmployeeName + @"'
+                    ,Department = '" + emp.Department + @"'
+                    ,DateOfJoining = '" + emp.DateOfJoining + @"'
+                    ,PhotoFileName = '" + emp.PhotoFileName + @"'
+                    where EmployeeId = " + emp.EmployeeId + @" 
+                    ";
+            DataTable dt = SelectUpdateDelete(query);
+            return new JsonResult("Updated Successfully");
+        }
+
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                    delete from dbo.Employee
+                    where EmployeeId = " + id + @" 
+                    ";
+            DataTable dt = SelectUpdateDelete(query);
+            return new JsonResult("Deleted Successfully");
+        }
+
+
+        [Route("SaveFile")]
+        [HttpPost]
+        public JsonResult SaveFile()
+        {
+            try
+            {
+                var httpRequest = Request.Form;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = _env.ContentRootPath + "/Photos/" + filename;
+
+                if (!Directory.Exists(_env.ContentRootPath + "//Photos//"))
+                {
+                    Directory.CreateDirectory(_env.ContentRootPath + "//Photos//");
+                }
+
+                using (var stream = new FileStream(physicalPath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+
+                return new JsonResult(filename);
+            }
+            catch (Exception)
+            {
+
+                return new JsonResult("img1.png");
+            }
+        }
+
+
+        [Route("GetAllDepartmentNames")]
+        public JsonResult GetAllDepartmentNames()
+        {
+            string query = @"
+                    select DepartmentName from dbo.Department
+                    ";
+
+            DataTable dt = SelectUpdateDelete(query);
+            return new JsonResult(dt);
+        }
+
+        public DataTable SelectUpdateDelete(string query)
+        {
+            DataTable table = new DataTable();
+            try
+            {
+                string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+                SqlDataReader myReader;
+                using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+                {
+                    myCon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader); ;
+
+                        myReader.Close();
+                        myCon.Close();
+                    }
+                }
+                return table;
+            }
+            catch (Exception)
+            {
+                return table;
+            }
+
+        }
+
+    }
+}
